910c18933558a4bb702890c07a970dfaae942cf0
Austin Flatt <austindflatt@gmail.com>
2021-05-24 15:13:40 -0400

    insert node into tree

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from linked_list import LinkedList
+import binary
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    linked_list = LinkedList()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    linked_list.prepend_node(55)
+    linked_list.prepend_node(60)
+    linked_list.prepend_node(65)
+
+binary.binary_search_tree()
