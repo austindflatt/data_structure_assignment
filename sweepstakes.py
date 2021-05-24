de02f4d17e4f722191ff2cf8649f0c3e037c9cdd
Austin Flatt <austindflatt@gmail.com>
2021-05-24 14:40:47 -0400

    added contestants to data structure and use randint to generate random winner and use print statement to print winners first and last name to the console

---
+++ sweepstakes.py
@@ -0,0 +1,14 @@
+import random
+
+sweepstakes_contestants = {
+
+
+    1: 'Austin Flatt',
+    2: 'Ella Anderson',
+    3: 'Hali Flatt',
+    4: 'Tre Tardy',
+    5: 'Aaron'
+}
+
+
+print(sweepstakes_contestants[random.randint(0, 4)])
