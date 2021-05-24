d972f10b25c8d72566bc2d1359993c4ff5a21da2
Austin Flatt <austindflatt@gmail.com>
2021-05-24 14:30:01 -0400

    created data structure for locations where I've celebrated my birthday and iterated the collection of locations and wrote code to print it to the console

---
+++ structure_implementation.py
@@ -0,0 +1,17 @@
+
+months = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
+          'December')
+
+print(months[2])
+
+birthday_locations = ['home', 'miami', 'california', 'atlanta', 'nashville']
+
+birthday_locations.extend(['Puerto Rico', 'New York', 'Los Cabos'])
+
+print(birthday_locations)
+
+i = 0
+
+while i < len(birthday_locations)
+    print(birthday_locations[i])
+    i += 1
